ea07b054af18a27d5852ec7fa8820920ebbc7735
rccyx <oss@rccyx.com>
2026-06-21 12:28:10 +0100

    update flake

--- flake.nix
+++ flake.nix
@@ -24,6 +24,38 @@
           thyx = self.packages.${system}.default;
         });
 
+      checks = forAllSystems (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          package = self.packages.${system}.default;
+
+          moduleEval = nixpkgs.lib.nixosSystem {
+            inherit system;
+
+            modules = [
+              self.nixosModules.default
+
+              {
+                services.displayManager.sddm.thyx.enable = true;
+                services.displayManager.sddm.wayland.enable = true;
+              }
+            ];
+          };
+        in
+        {
+          package = package;
+
+          nixos-module = pkgs.runCommand "thyx-nixos-module-check" { } ''
+            test "${moduleEval.config.services.displayManager.sddm.theme}" = "thyx"
+            test -e "${package}/share/sddm/themes/thyx/metadata.desktop"
+            test -e "${package}/share/sddm/themes/thyx/theme.conf"
+            test -e "${package}/share/sddm/themes/thyx/src/Main.qml"
+            test -e "${package}/share/fonts/truetype/thyx/PlusJakartaSans-VariableFont_wght.ttf"
+
+            touch "$out"
+          '';
+        });
+
       nixosModules = {
         default = import ./module.nix;
         thyx = self.nixosModules.default;
